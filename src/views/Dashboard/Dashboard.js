f55645501751dd2dcd7011bed40e20ecd8722390
Unknown <ibrahim.a.hamid@gmail.com>
2018-12-12 13:25:09 +0200

    Added Animations (Clock - Candles - Water)

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -1,9 +1,18 @@
 import React, {Component} from 'react';
+import Clock from 'react-live-clock';
+import Candles from "../Animations/Candles/Candles";
+import {Container, Jumbotron} from "reactstrap";
 
 class Dashboard extends Component {
-    render () {
+    render() {
         return (
             <div>
+                <h1><Clock format={'hh:mm:ss'} ticking={true} timezone={'Africa/Cairo'}/></h1>
+                <Jumbotron fluid>
+                    <Container fluid>
+                        <Candles/>
+                    </Container>
+                </Jumbotron>
             </div>
         );
     }
