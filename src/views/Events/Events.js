351c93bc4c14eb176428f3a01c3fcec65061d746
Unknown <ibrahim.a.hamid@gmail.com>
2018-12-19 23:21:07 +0200

    Added "Fab" to events

--- src/views/Events/Events.js
+++ src/views/Events/Events.js
@@ -2,13 +2,22 @@ import React, {Component} from "react";
 import { VerticalTimeline }  from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
 import Event from "./Event/Event";
+import { Fab, Action } from 'react-tiny-fab';
+import 'react-tiny-fab/dist/styles.min.css';
+import {FaPlus, FaUsers} from 'react-icons/fa';
 
 class Events extends Component {
 	render() {
 		return (
-			<VerticalTimeline layout="1-column">
-				<Event/>
-			</VerticalTimeline>
+			<div>
+                <Fab icon={<FaPlus />} event="click">
+                    <Action
+						text="New Event"><FaUsers/></Action>
+                </Fab>
+				<VerticalTimeline layout="1-column">
+					<Event/>
+				</VerticalTimeline>
+			</div>
 		);
 	}
 }
